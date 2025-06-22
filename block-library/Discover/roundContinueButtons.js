4f19d813a9ed31bad8625000e97dd796ffd309d2
Nicholas Burns <51179768+hfxnick@users.noreply.github.com>
2025-06-22 12:38:37 -0300

    More library work

--- block-library/Discover/roundContinueButtons.js
+++
@@ -1,9 +0,0 @@
-// Round Continue Buttons Feature for Rise
-(function() {
-    const continueButton_BorderRadius = '50px';
-    const finalCustomCSS = `.continue-btn { border-radius: ${continueButton_BorderRadius} !important; }`;
-    const styleSheet = document.createElement("style");
-    styleSheet.type = "text/css";
-    styleSheet.innerText = finalCustomCSS;
-    document.head.appendChild(styleSheet);
-})();
